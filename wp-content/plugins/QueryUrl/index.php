65e7cade26f0cd1988dfe8f6ba7580907bbcd4d1
Nadar Rossano <nadarr@toyga.com>
2020-02-18 18:42:32 +0200

    adding footer:php, css

--- wp-content/plugins/QueryUrl/index.php
+++ wp-content/plugins/QueryUrl/index.php
@@ -9,15 +9,30 @@ Text Domain: Qurey Url
 */
 
 // adding js to the plugin
-
 function add_js(){
    wp_enqueue_script('add_js', plugins_url( '/main.js', __FILE__ ));
 }
 add_action('init', 'add_js');
+
+// adding css to the plugin
+function add_css(){
+  wp_enqueue_style('add_css', plugins_url( '/style.css', __FILE__ ));
+}
+add_action('init', 'add_css');
+
+// Footer section 
+function footer_wp_test() {
+  echo '<footer class="footer-wrapper"> here is the wp-test-footer </footer> ';
+
+}
+add_action( 'wp_footer', 'footer_wp_test' );
 
 ?>
 
 <script type="text/javascript">
     window.alert("alert");
 </script>
+
+
+
 
