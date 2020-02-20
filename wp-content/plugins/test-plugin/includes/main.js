cad3dc6543b8a3e33d9b899197b6aaa5d694d0fe
Nadar Rossano <nadarr@toyga.com>
2020-02-20 15:32:08 +0200

    adding css and js wp_script

--- wp-content/plugins/test-plugin/includes/main.js
+++ wp-content/plugins/test-plugin/includes/main.js
@@ -1,15 +1,5 @@
-
-alert('lllll');
-
 function getQueryStringParameter(param) {
-  console.log('here');
   const query = window.location.search;
   const urlParams = new URLSearchParams(query);
-
   return urlParams.get(param);
 }
-
-alert('hhhhh');
-
-
-
---
+++ wp-content/plugins/test-plugin/test-plugin.php
@@ -0,0 +1,25 @@
+<?php 
+/*
+Plugin Name: Test plugin
+Description: test
+Version: 1.0
+Author: Nadar Rossano 
+*/
+
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
+/**
+ * Register custom js and css files
+ *
+ * @return void
+ */
+function register_custom_files() {
+    wp_enqueue_style( 'main-style', plugin_dir_url(__FILE__). '/includes/style.css' );
+    wp_enqueue_script( 'main-script', plugin_dir_url(__FILE__). '/includes/main.js' );
+}
+
+add_action('wp_enqueue_scripts', 'register_custom_files');
